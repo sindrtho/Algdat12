b73759061517e1f9d119776d0a94f971ce9a50db
anettosi <sukkerbombe@hotmail.com>
2018-11-09 13:10:36 +0100

    nesten der, får ut rett 10101 streng

--- src/LesFrekvensTest.java
+++ src/LesFrekvensTest.java
@@ -10,27 +10,27 @@ public class LesFrekvensTest {
 
     /*
     innpakining:
-tegn:32, verdi: 1, kode: 0000
-tegn:98, verdi: 1, kode: 0001
+tegn:32, verdi: 1, kode: 1110
+tegn:98, verdi: 1, kode: 1111
 tegn:101, verdi: 1, kode: 0010
 tegn:103, verdi: 1, kode: 0011
-tegn:110, verdi: 1, kode: 0100
-tegn:115, verdi: 1, kode: 0101
-tegn:105, verdi: 2, kode: 0110
-tegn:108, verdi: 2, kode: 0111
-tegn:121, verdi: 2, kode: 10
-tegn:99, verdi: 4, kode: 11
+tegn:110, verdi: 1, kode: 1100
+tegn:115, verdi: 1, kode: 1101
+tegn:105, verdi: 2, kode: 010
+tegn:108, verdi: 2, kode: 011
+tegn:121, verdi: 2, kode: 000
+tegn:99, verdi: 4, kode: 10
 
 utpakning:
-tegn:32, verdi: 1, kode: 0000
-tegn:98, verdi: 1, kode: 0001
+tegn:32, verdi: 1, kode: 1110
+tegn:98, verdi: 1, kode: 1111
 tegn:101, verdi: 1, kode: 0010
 tegn:103, verdi: 1, kode: 0011
-tegn:110, verdi: 1, kode: 0100
-tegn:115, verdi: 1, kode: 0101
-tegn:105, verdi: 2, kode: 0110
-tegn:108, verdi: 2, kode: 0111
-tegn:121, verdi: 2, kode: 10
-tegn:99, verdi: 4, kode: 11
+tegn:110, verdi: 1, kode: 1100
+tegn:115, verdi: 1, kode: 1101
+tegn:105, verdi: 2, kode: 010
+tegn:108, verdi: 2, kode: 011
+tegn:121, verdi: 2, kode: 000
+tegn:99, verdi: 4, kode: 10
      */
 }
